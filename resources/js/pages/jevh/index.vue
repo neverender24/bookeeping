415e16e62e8017d7d38e6bf477221afbfde133c6
nyanya29 <ralphjadegarcia29@gmail.com>
2022-03-17 15:50:05 +0800

    updated the action buttons

--- resources/js/pages/jevh/index.vue
+++ resources/js/pages/jevh/index.vue
@@ -71,23 +71,20 @@
                                     <td>{{ item.FPAYEE }}</td>
                                     <td>{{ item.FREMK }}</td>
                                     <td>
-                                        
-                                            <a class="dropdown-item" @click="show_details(item)">
-                                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye me-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                    <path fill-rule="evenodd" d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.134 13.134 0 0 0 1.66 2.043C4.12 11.332 5.88 12.5 8 12.5c2.12 0 3.879-1.168 5.168-2.457A13.134 13.134 0 0 0 14.828 8a13.133 13.133 0 0 0-1.66-2.043C11.879 4.668 10.119 3.5 8 3.5c-2.12 0-3.879 1.168-5.168 2.457A13.133 13.133 0 0 0 1.172 8z"></path>
-                                                    <path fill-rule="evenodd" d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"></path>
-                                                </svg>View
+                                        <div class="btn btn-group">
+                                            <a class="dropdown-item p-1" @click="show_details(item)">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                                </svg>
                                             </a>
-                                            </td>
-                                            <td>
-                                            
-                                                <a class="dropdown-item " @click="print_report(item)">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-printer" viewBox="0 0 16 16">
-                                                        <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
-                                                        <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2H5zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4V3zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2H5zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1z"/>
-                                                    </svg>
-                                                </a>
-                                      
+                                            <a class="dropdown-item p-1" @click="print_report(item)">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
+                                                    <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z"/>
+                                                    <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+                                                </svg>
+                                            </a>
+                                        </div>
                                     </td>
                                 </tr>
                             </tbody>
@@ -164,15 +161,14 @@ export default {
 
         let columns = [
             
-            { width: "5%", label: "Fiscal Year", name: "fiscalyear" },
-            { width: "10%", label: "Fund Detail Code", name: "FUND_SCODE" },
+            { width: "10%", label: "Fiscal Year", name: "fiscalyear" },
+            { width: "15%", label: "Fund Detail Code", name: "FUND_SCODE" },
             { width: "10%", label: "JEV Number", name: "FJEVNO " },
             { width: "10%", label: "JEV Date", name: "FJEVDATE " },
             { width: "10%", label: "Check No.", name: ".FCHKNO " },
             { width: "10%", label: "Payee", name: "FPAYEE" },
-            { width: "20", label: "Particulars", name: "FREMK" },
-            { width: "0%", label: "Action", name: "action" }, 
-            { width: "0%", label: "", name: "" }, 
+            { width: "15", label: "Particulars", name: "FREMK" },
+            { width: "10%", label: "Action", name: "action" }, 
         ];
 
         //end of pagination variables
